5b8d185a5f38ece068c7125f300044ad6fafc0ce
Donald Iloekwe <donytex4u@gmail.com>
2023-03-24 17:09:34 +0000

    add navbar section + responsive navbar

---
+++ src/layouts/Navbar.tsx
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+import Link from 'next/link';
+import { MdLocationOn } from 'react-icons/md';
+import { ImFacebook, ImInstagram, ImTwitter } from 'react-icons/im';
+
+const Navbar = () => {
+  const [activeMobileMenu, setActiveMobileMenu] = useState(false);
+  return (
+    <div className='navbar'>
+      <div className='navbar__wrapper'>
+        <div className='navbar__topBar'>
+          <div className='navbar__topBarContainer'>
+            <div className='navbar__topBarLocation'>
+              <span className='navbar__address'>
+                <MdLocationOn className='navbar__icon' />
+                Plot 596 Independence Avenue, Central Business District, Abuja
+              </span>
+            </div>
+            <div className='navbar__topBarRight'>
+              <div className='navbar__socials'>
+                <Link href='#'>
+                  <ImTwitter size={15} />
+                </Link>
+                <Link href='#'>
+                  <ImFacebook size={15} />
+                </Link>
+                <Link href='#'>
+                  <ImInstagram size={14} />
+                </Link>
+              </div>
+              <div className='navbar__button'>
+                <Link href='#'>consult for free</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='navbar__main'>
+          <div className='navbar__container'>
+            <Link href={'/'} className='navbar__logo'>
+              <img src='/logo.png' alt='logo' />
+            </Link>
+            <div className='navbar__menu'>
+              <Link href='#'>about us</Link>
+              <span>|</span>
+              <Link href='#'>Articles</Link>
+              <span>|</span>
+              <Link href='#'>practice areas</Link>
+              <span>|</span>
+              <Link href='#'>awards</Link>
+              <span>|</span>
+              <Link href='#'>contact</Link>
+            </div>
+            <div className='navbar__mainMobile'>
+              <div className='navbar__mainSocials'>
+                <Link href='#'>
+                  <ImTwitter />
+                </Link>
+                <Link href='#'>
+                  <ImFacebook />
+                </Link>
+                <Link href='#'>
+                  <ImInstagram />
+                </Link>
+              </div>
+
+              <div
+                className={`navbar__hamburger ${
+                  activeMobileMenu && 'navbar__hamburger--active'
+                }`}
+                onClick={() => setActiveMobileMenu(!activeMobileMenu)}
+              >
+                <span className='navbar__hamburger--line'></span>
+                <span className='navbar__hamburger--line'></span>
+                <span className='navbar__hamburger--line'></span>
+              </div>
+
+              <div
+                className={`navbar__hamburgerMenu ${
+                  activeMobileMenu && 'navbar__hamburgerMenu--active'
+                }`}
+              >
+                <Link href='#'>about us</Link>
+                <span className='navbar__dividerMobile'></span>
+                <Link href='#'>Articles</Link>
+                <span className='navbar__dividerMobile'></span>
+                <Link href='#'>practice areas</Link>
+                <span className='navbar__dividerMobile'></span>
+                <Link href='#'>awards</Link>
+                <span className='navbar__dividerMobile'></span>
+                <Link href='#'>contact</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Navbar from '@/layouts/Navbar';
 import Head from 'next/head';
 
 export default function Home() {
@@ -10,9 +11,7 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <main>
-        <div>
-          <p>Welcome to Home.</p>
-        </div>
+        <div></div>
       </main>
     </>
   );
