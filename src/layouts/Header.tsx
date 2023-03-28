3584dd04e3cb91af114bf88c51c60a4f9d50657e
Donald Iloekwe <donytex4u@gmail.com>
2023-03-28 16:58:22 +0100

    add framer motion animation to header content

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -3,6 +3,8 @@ import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 import { Carousel } from '../../typings';
 import { SlArrowLeft, SlArrowRight } from 'react-icons/sl';
+import { motion } from 'framer-motion';
+import { useSwipeable } from 'react-swipeable';
 
 const Header = () => {
   const [activeIndex, setActiveIndex] = useState(1);
@@ -16,6 +18,28 @@ const Header = () => {
         "When you go into court you are putting your fate into the hands of twelve people who weren't smart enough to get out of jury duty.",
       image: '/images/Slide1.jpg',
       button: 'Read More',
+      animation: {
+        hidden: {
+          opacity: 0,
+          y: 10,
+        },
+        show: {
+          opacity: 1,
+          y: 0,
+          transition: {
+            ease: [0.2, 0.05, 0.05, 0.01],
+            duration: 1,
+          },
+        },
+        exit: {
+          opacity: 0,
+          y: -50,
+          transition: {
+            ease: 'easeInOut',
+            duration: 0.8,
+          },
+        },
+      },
     },
     {
       id: 2,
@@ -25,6 +49,28 @@ const Header = () => {
         "When you go into court you are putting your fate into the hands of twelve people who weren't smart enough to get out of jury duty.",
       image: '/images/Slide2.jpg',
       button: 'Read More',
+      animation: {
+        hidden: {
+          opacity: 0,
+          scale: 0.8,
+        },
+        show: {
+          opacity: 1,
+          scale: 1,
+          transition: {
+            ease: [0.2, 0.05, 0.05, 0.01],
+            duration: 1,
+          },
+        },
+        exit: {
+          opacity: 0,
+          scale: 1.2,
+          transition: {
+            ease: 'easeInOut',
+            duration: 0.8,
+          },
+        },
+      },
     },
     {
       id: 3,
@@ -34,8 +80,38 @@ const Header = () => {
         "When you go into court you are putting your fate into the hands of twelve people who weren't smart enough to get out of jury duty.",
       image: '/images/Slide3.jpg',
       button: 'Read More',
+      animation: {
+        hidden: {
+          opacity: 0,
+          x: -100,
+        },
+        show: {
+          opacity: 1,
+          x: 0,
+          transition: {
+            ease: [0.2, 0.05, 0.05, 0.01],
+            duration: 1,
+          },
+        },
+        exit: {
+          opacity: 0,
+          x: -100,
+          transition: {
+            ease: 'easeInOut',
+            duration: 0.8,
+          },
+        },
+      },
     },
   ];
+
+  const handlers = useSwipeable({
+    onSwipedLeft: () => handleSlideLeft(),
+    onSwipedRight: () => handleSlideRight(),
+    swipeDuration: 500,
+    preventScrollOnSwipe: true,
+    trackMouse: true,
+  });
 
   const handleSlideLeft = (): void => {
     setActiveIndex(activeIndex - 1);
@@ -67,9 +143,17 @@ const Header = () => {
     };
   }, [activeIndex]);
 
+  const container = {
+    show: {
+      transition: {
+        staggerChildren: 0.4,
+      },
+    },
+  };
+
   return (
     <section className='header'>
-      <div className='header__carousel'>
+      <div className='header__carousel' {...handlers}>
         {carouselContent.map((slide) => (
           <div
             key={slide.id}
@@ -84,16 +168,34 @@ const Header = () => {
               alt='header-carousel-image'
               className='header__carouselItemImage'
             />
-            <div className='header__carouselItemContent'>
-              <h2 className='header__carouselItemTitle'>{slide.title}</h2>
-              <h2 className='header__carouselItemSubtitle'>{slide.subtitle}</h2>
-              <h2 className='header__carouselItemDescription'>
-                {slide.description}
-              </h2>
-              <Link href='#' className='header__carouselItemButton'>
-                {slide.button}
-              </Link>
-            </div>
+            <motion.div
+              variants={container}
+              initial='hidden'
+              animate='show'
+              exit='exit'
+              key={activeIndex}
+            >
+              <div className='header__carouselItemContent'>
+                <motion.div variants={slide.animation}>
+                  <h2 className='header__carouselItemTitle'>{slide.title}</h2>
+                </motion.div>
+                <motion.div variants={slide.animation}>
+                  <h2 className='header__carouselItemSubtitle'>
+                    {slide.subtitle}
+                  </h2>
+                </motion.div>
+                <motion.div variants={slide.animation}>
+                  <h2 className='header__carouselItemDescription'>
+                    {slide.description}
+                  </h2>
+                </motion.div>
+                <motion.div variants={slide.animation}>
+                  <Link href='#' className='header__carouselItemButton'>
+                    {slide.button}
+                  </Link>
+                </motion.div>
+              </div>
+            </motion.div>
           </div>
         ))}
       </div>
--- typings.d.ts
+++ typings.d.ts
@@ -1,11 +1,13 @@
-interface CarouselItem {
+import { Variants } from 'framer-motion';
+
+export interface CarouselItem {
   id: number;
   title: string;
   subtitle: string;
   description: string;
   image: string;
   button?: string;
+  animation?: Variants;
 }
-[];
 
 export interface Carousel extends Array<CarouselItem> {}
