6e21cbab602f5278d3946986be69cf4de2347007
Donald Iloekwe <donytex4u@gmail.com>
2023-05-08 09:15:20 +0100

    change pactice card design

--- src/components/PracticeCard.tsx
+++ src/components/PracticeCard.tsx
@@ -14,14 +14,16 @@ const PracticeCard = (practice: PracticeItem) => {
           fill
         />
         <div className='practice__cardOverlay'>
-          <div className='practice__bodyHeader'>
-            <span className='practice__bodyIcon'>{icon}</span>
-            <h2 className='practice__title'>{title}</h2>
-          </div>
-          <p className='practice__description'>{description}</p>
-          <a href={link} className='practice__link'>
-            Read More
-          </a>
+          <>
+            <div className='practice__bodyHeader'>
+              <span className='practice__bodyIcon'>{icon}</span>
+              <h2 className='practice__title'>{title}</h2>
+            </div>
+            <p className='practice__description'>{description}</p>
+            <a href={link} className='practice__link'>
+              Read More
+            </a>
+          </>
         </div>
       </div>
     </>
